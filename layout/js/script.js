b3f2324bde9e6d896d107a05f6c8963fa215f5d2
MSDN-SPECIAL\Administrator <rewq930@gamil.com>
2017-04-23 13:12:38 +0900

    no message

--- layout/js/script.js
+++ layout/js/script.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
     
+    // mouseenter mouseleave
+    // mouseover mouseout
+    // 
+    $('.menu .drop a').on('mouseenter', function(){
+//       $('.sub').stop().slideDown();
+        // 기능구현
+       /* $(this).siblings('.sub').show();    // 선택한 메뉴의 .sub만 선택
+        $('.sub').animate({ top:'100%', opacity:1 });*/
+        
+        $(this).siblings('.sub').addClass('active');
+        $(this).addClass('on');
+    });
+    
+    $('.menu .drop').on('mouseleave', function(){
+//       $('.sub').stop().slideUp(); 
+        
+        // 기능구현
+        /*$(this).children('.sub').animate({ top:'120%', opacity:0 }, function(){
+            $('.sub').hide();   // animation 실행후 hide 해주기위해
+        });*/ 
+        
+        $(this).children('.sub').removeClass('active');
+        $(this).children('a').removeClass('on');
+    });
 });
--- project/js/script.js
+++ project/js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-   
+    $('.menu_lv1 .drop a').on('mouseenter', function(){
+        $('.menu_lv2').stop().slideDown();
+        $(this).addClass('on');
+    });
+    
+    $('.menu_lv1 .drop').on('mouseleave', function(){
+        $('.menu_lv2').stop().slideUp(); 
+        $(this).children('a').removeClass('on');
+    });
 });
